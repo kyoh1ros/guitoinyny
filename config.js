cf14016163433bb906e76fc832d78a85b6a33be7
kyoh1ros <116280565+kyoh1ros@users.noreply.github.com>
2024-03-11 00:23:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
   mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
   messDesc: "Đi cùng anh đến tận cùng thời gian nhé babiii :3",
   btnAccept: "Okiiiii lun <3",
-  messLink: "m.me/hoanggvuu123", //link mess của các bạn. VD: https://m.me/nam.nodemy
+  messLink: "https://m.me/hoanggvuu123", //link mess của các bạn. VD: https://m.me/nam.nodemy
 };
